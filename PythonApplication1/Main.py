2389186a99a27d43f263be1d6faf439408e26003
Amy Harrison <amy.harrison@capita.co.uk>
2020-01-30 08:54:16 +0000

    streamlined imports

--- PythonApplication1/Main.py
+++ PythonApplication1/Main.py
@@ -7,32 +7,34 @@
 #		comp tested: 29/01/2020
 #		Author: AH
 #-----------------------------------------------------------------------
-import os
-import cv2 as cv
 import numpy as np
-import random as rand
-import statistics as stat
 import matplotlib.pyplot as plt 
 
 from datetime import datetime
-
+from cv2 import(
+	FastFeatureDetector_create,
+	ORB_create,
+	SimpleBlobDetector_create,
+	VideoCapture,
+	waitKey)
 #initialise camera
 try:
-	cam = cv.VideoCapture(0)
+	cam = VideoCapture(0)
 except:
 	cam = "Failed"
+	
 intCount = 0
-intPwdLength = 4000
+intPwdLength = 32*100
 strPassword = ""
 
-fast = cv.FastFeatureDetector_create()
-orb = cv.ORB_create()
-blob = cv.SimpleBlobDetector_create()
+fast = FastFeatureDetector_create()
+orb = ORB_create()
+blob = SimpleBlobDetector_create()
 
 arryPlotX = np.zeros(254)
 arryPlotY = np.zeros(254)
-for i in range(0, 254-32):
-	arryPlotY[i] = i+32
+for i in range(0, 254):
+	arryPlotY[i] = i
 
 print(datetime.now())
 
@@ -60,31 +62,23 @@ for intCount in range(0, intPwdLength):
 	#initialise seed
 	#				R					G					B
 	intSeed = int(intCh0) << 32 | int(intCh1) << 16 | int(intCh2) << 0
-
-	#print(intSeed)
-	#rand.seed(intSeed)
-
-	#create random letter
-	#letter = rand.randint(32,(254-33))
 	
 	letter = (intSeed%254)
 	#print(letter)
 	
 	#add letter to array 
-	if letter == 32:
-		strPassword += str(chr(letter+1))
-	else:
-		strPassword += str(chr(letter))
-	cv.waitKey(1)
+	strPassword += str(chr(letter))
+	waitKey(1)
 
 	arryPlotX[letter] += 1
+	
+print(datetime.now())
 
 plt.bar(arryPlotY, arryPlotX)
 plt.show()
 
 print(arryPlotX)
 
-print(strPassword)
+#print(strPassword)
 
-print(datetime.now())
 
